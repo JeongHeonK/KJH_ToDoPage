96266e6691b0be0c23dd47ffcec93ce03c4e64a1
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-11 23:11:15 +0900

    feat: Homepage 수정

---
+++ app/components/HomeScene.tsx
@@ -0,0 +1,3 @@
+export default function HomeScene() {
+  return <div>Kanban</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import HomeScene from "./components/HomeScene";
+
 export default function Home() {
-  return (
-    <div className="w-full">
-      to-do-app
-      <span>허스키 테스트</span>
-    </div>
-  );
+  return <HomeScene />;
 }
