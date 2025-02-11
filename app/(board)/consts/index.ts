d9b5d3d76aa5448369bd522e741facf428e8f1e2
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-11 18:55:05 +0900

    feat: color 상수 생성

---
+++ app/(board)/consts/index.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  slate: "bg-slate-300",
+  rose: "bg-rose-300",
+  fuchsia: "bg-fuchsia-300",
+  teal: "bg-teal-300",
+  yellow: "bg-yellow-200",
+  orange: "bg-orange-300",
+  red: "bg-red-300",
+};
