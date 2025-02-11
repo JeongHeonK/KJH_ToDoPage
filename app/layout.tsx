4ce54a048f90570be151bd3f4a24b8ccdab03718
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-11 23:27:17 +0900

    style: layout style 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className={`${notoSans.className} antialiased`}>
         <Header />
-        <main className="bg-slate-50 h-screen px-2 pt-14">{children}</main>
+        <main className="bg-zinc-100 h-screen px-2 pt-16">{children}</main>
       </body>
     </html>
   );
