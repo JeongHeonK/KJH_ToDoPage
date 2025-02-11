bd8c92183f0ea6f4d9671d3804dde4eb4e879c66
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-11 18:04:30 +0900

    feat: metadata 추가

--- app/(board)/board/page.tsx
+++ app/(board)/board/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Board",
+  description: "board를 조회하고 편집할 수 있습니다.",
+};
+
 export default function BoardPage() {
   return <div className="bg-zinc-100 m-2">to-do-app(board)</div>;
 }
