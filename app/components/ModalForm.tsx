f828e7e7314257ce89f56047f01faa0072a039a1
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-12 02:16:02 +0900

    feat: color-picker연결

--- app/components/ModalForm.tsx
+++ app/components/ModalForm.tsx
@@ -13,20 +13,13 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
+import { TwitterPicker } from "react-color";
 import { formSchema } from "../util/validation";
+import { useModalStore } from "../store";
 
 export default function ModalForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-    },
-  });
-
-  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (data) => {
-    console.log(data);
-  };
+  const { form, handleChange, onSubmit } = useKanban();
 
   return (
     <Form {...form}>
@@ -48,8 +41,41 @@ export default function ModalForm() {
             </FormItem>
           )}
         />
+        <TwitterPicker className="mx-auto rounded-lg" onChange={handleChange} />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
   );
 }
+
+const useKanban = () => {
+  const onClose = useModalStore((state) => state.handleClose);
+  const [color, setColor] = useState("#22194D");
+  const form = useForm<z.infer<typeof formSchema> & { color: string }>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const handleChange = <T extends { hex: string }>(color: T) => {
+    setColor(color.hex);
+  };
+
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (data) => {
+    if (!window) return;
+    const kanban = window.localStorage.getItem("board");
+    if (!kanban) {
+      window.localStorage.setItem(
+        "board",
+        JSON.stringify([{ title: data.title, color }]),
+      );
+    } else {
+      const kanbanArr = JSON.parse(kanban);
+      kanbanArr.push({ title: data.title, color });
+      window.localStorage.setItem("board", JSON.stringify(kanbanArr));
+    }
+    onClose();
+  };
+  return { form, handleChange, onSubmit };
+};
