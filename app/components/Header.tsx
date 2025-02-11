021bd66e5c517540a170da27105c834ff10599ab
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-11 21:10:51 +0900

    feat: modal 동작 확인

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,16 +2,27 @@
 
 import { usePathname } from "next/navigation";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { useModalStore } from "../store";
 
 export default function Header() {
   const pathName = usePathname();
-  const decodedPathName = decodeURI(pathName);
-  const path = decodedPathName.slice(1).split("/").join(" > ");
+  const decodedPathName = decodePathName(pathName);
+  const handleOpen = useModalStore((state) => state.handleOpen);
 
   return (
-    <header className="flex items-center gap-2 pt-2">
+    <header className="flex items-center justify-start gap-2 pt-3 pb-2 w-full">
       <SidebarTrigger />
-      <span className="text-sm -mt-1"> todo &gt; {path}</span>
+      <span className="text-sm -mt-1"> todo &gt; {decodedPathName}</span>
+      <Button onClick={handleOpen} className="absolute right-3 top-2 text-sm">
+        보드 생성
+      </Button>
     </header>
   );
 }
+
+const decodePathName = (path: string) => {
+  const decodedPathName = decodeURI(path).slice(1).split("/").join(" > ");
+
+  return decodedPathName;
+};
--- app/components/ModalWrapper.tsx
+++ app/components/ModalWrapper.tsx
@@ -8,10 +8,16 @@ export default function ModalWrapper<P extends JSX.IntrinsicAttributes>(
 ) {
   return function Wrapper(props: P) {
     const isOpen = useModalStore((state) => state.isOpen);
+    const handleClose = useModalStore((state) => state.handleClose);
     return (
       <>
         {isOpen ? (
-          <div className="bg-black/60 fixed top-0 left-0 right-0 bottom-0 z-30">
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={handleClose}
+            className="bg-black/60 fixed top-0 left-0 right-0 bottom-0 z-30"
+          >
             <WrappedComponent {...props} />
           </div>
         ) : null}
