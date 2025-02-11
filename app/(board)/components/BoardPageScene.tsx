65ca13147114b9dca72ee4800b1da043c9c7ea30
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-11 18:55:28 +0900

    feat: mockData 생성 및 연결

--- app/(board)/components/BoardPageScene.tsx
+++ app/(board)/components/BoardPageScene.tsx
@@ -1,3 +1,44 @@
+import BoardItem from "./BoardItem";
+import BoardWrapper from "./BoardWrapper";
+import { COLORS } from "../consts";
+
+const mockData = [
+  {
+    title: "할일 1",
+    color: COLORS.fuchsia,
+  },
+  {
+    title: "할일 2",
+    color: COLORS.orange,
+  },
+  {
+    title: "할일 3",
+    color: COLORS.red,
+  },
+  {
+    title: "할일 4",
+    color: COLORS.rose,
+  },
+  {
+    title: "할일 5",
+    color: COLORS.slate,
+  },
+  {
+    title: "할일 6",
+    color: COLORS.teal,
+  },
+  {
+    title: "할일 7",
+    color: COLORS.yellow,
+  },
+];
+
 export default function BoardPageScene() {
-  return <div className="bg-zinc-100 m-2">to-do-app(board)</div>;
+  return (
+    <BoardWrapper>
+      {mockData.map((item) => (
+        <BoardItem key={item.title} title={item.title} color={item.color} />
+      ))}
+    </BoardWrapper>
+  );
 }
