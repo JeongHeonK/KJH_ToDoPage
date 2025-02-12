acaa2e91974b457feac45834fd5dd9a2839b59d2
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-12 13:33:28 +0900

    feat: 컴포넌트 분리

---
+++ app/components/BoardTitle.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "@/components/ui/badge";
+import FlexSpace from "./FlexSpace";
+
+interface BoardTitleProps {
+  title?: string;
+  color: string;
+}
+
+export default function BoardTitle({ color, title }: BoardTitleProps) {
+  return (
+    <div className="flex items-center relative">
+      <Badge color={color} className="absolute top-0 -left-1">
+        {title}
+      </Badge>
+      <FlexSpace />
+      <button
+        style={{ background: color }}
+        className="px-2 py-0 rounded-full text-sm text-white"
+      >
+        +
+      </button>
+    </div>
+  );
+}
