c763af202a070426c7e1a974c230753a03826521
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-12 21:47:04 +0900

    feat: boardItem 수정

--- app/components/Board/BoardItem.tsx
+++ app/components/Board/BoardItem.tsx
@@ -1,14 +1,24 @@
+import { useTodoStore } from "@/app/store";
 import Todo from "../Todo/Todo";
+import TodoCreateButton from "../Todo/TodoCreateButton";
 import BoardTitle from "./BoardTitle";
 
 interface BoardItemProps {
   title?: string;
   color: string;
-  id: string;
+  boardId: string;
+  todoIds: string[];
 }
 
-export default function BoardItem({ id, title, color }: BoardItemProps) {
-  console.log(id);
+export default function BoardItem({
+  boardId,
+  title,
+  color,
+  todoIds,
+}: BoardItemProps) {
+  const todos = useTodoStore((state) => state.todos);
+  const boardTodos = todoIds?.map((id) => ({ ...todos[id], id }));
+  console.log(boardId);
   return (
     <div
       className="border-2 min-w-[250px] h-fit rounded-lg px-2 pt-1 pb-5 bg-white flex flex-col gap-3"
@@ -16,11 +26,8 @@ export default function BoardItem({ id, title, color }: BoardItemProps) {
       draggable
     >
       <BoardTitle title={title} color={color} />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
+      {boardTodos?.map((item) => <Todo key={item.id} />)}
+      <TodoCreateButton color={color} />
     </div>
   );
 }
