d6dbd1364a4924b8b1cf30b4770a96f3ef7102e2
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-12 17:46:43 +0900

    refactor: zustand action 수정

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,12 +5,16 @@ import FlexSpace from "./FlexSpace";
 import { useModalStore } from "../store";
 
 export default function Header() {
-  const handleOpen = useModalStore((state) => state.handleOpen);
+  const modalActions = useModalStore((state) => state.actions);
   return (
     <header className="p-3 bg-slate-200 text-sm fixed left-0 right-0 top-0 flex items-center">
       <span>Kanban Board</span>
       <FlexSpace />
-      <Button type="button" className="text-sm" onClick={handleOpen}>
+      <Button
+        type="button"
+        className="text-sm"
+        onClick={modalActions.handleOpen}
+      >
         보드 생성
       </Button>
     </header>
--- app/components/ModalForm.tsx
+++ app/components/ModalForm.tsx
@@ -50,7 +50,7 @@ export default function ModalForm() {
 }
 
 const useKanban = () => {
-  const onClose = useModalStore((state) => state.handleClose);
+  const modalActions = useModalStore((state) => state.actions);
   const addBoard = useBoardsStore((state) => state.addBoard);
   const [color, setColor] = useState("#22194D");
   const form = useForm<z.infer<typeof formSchema> & { color: string }>({
@@ -68,7 +68,7 @@ const useKanban = () => {
     const id = nanoid(8);
     const newData = { ...data, id, color, todoIds: [] };
     addBoard(newData);
-    onClose();
+    modalActions.handleClose();
   };
   return { form, handleChange, onSubmit };
 };
