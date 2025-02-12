a2fc566f8cf583388d36a0a31cad185bff2a0ad6
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-12 18:00:31 +0900

    chore: board 관련 컴포넌트 분리

--- app/components/Board/BoardModal.tsx
+++ app/components/Board/BoardModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useModalStore } from "../store";
-import ModalForm from "./ModalForm";
+import { useModalStore } from "../../store";
+import ModalForm from "../ModalForm";
 
 export default function BoardModal() {
   const isOpen = useModalStore((state) => state.isOpen);
