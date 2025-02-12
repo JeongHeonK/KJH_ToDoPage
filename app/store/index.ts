3e7ae07e7c73797c8205b51efb2421d47b55a337
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-12 10:12:41 +0900

    feat: boardStore 생성

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,6 +1,17 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { ModalState, ModalAction, createModalSlice } from "./slice/modalSlice";
+import { BoardState, BoardAction, createBoardsSlice } from "./slice/boardSlice";
 
 export const useModalStore = create<ModalState & ModalAction>()((...args) => ({
   ...createModalSlice(...args),
 }));
+
+export const useBoardsStore = create<BoardState & BoardAction>()(
+  persist(
+    (...args) => ({
+      ...createBoardsSlice(...args),
+    }),
+    { name: "boards-storage" },
+  ),
+);
