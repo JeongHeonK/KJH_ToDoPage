a4b5c35e27a92b2bef38b31b7cfcd1d72d88755f
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-13 14:37:39 +0900

    style: toast style 수정

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -19,7 +19,11 @@ export function Toaster() {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
+              {title && (
+                <ToastTitle className="text-red-500 font-medium">
+                  {title}
+                </ToastTitle>
+              )}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
               )}
