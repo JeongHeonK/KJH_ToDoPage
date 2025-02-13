d2f4c8d8245288acc8f37139ecb2824514e1dd9a
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-13 11:41:49 +0900

    refactor: boardWrapper 컴포넌트 분리
    - 구현상세 제거

---
+++ app/components/Board/BoardItemWrapper.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from "react";
+import * as motion from "motion/react-client";
+
+export default function BoardItemWrapper({
+  children,
+  color,
+}: PropsWithChildren<{ color: string }>) {
+  return (
+    <motion.div
+      style={{ border: `2px solid ${color}` }}
+      initial={{ opacity: 0, scale: 0.7 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
+      transition={{ duration: 0.4, ease: "easeInOut" }}
+      className="border-2 min-w-[250px] h-fit rounded-lg px-2 pt-1 pb-5 bg-white flex flex-col gap-3"
+      draggable
+    >
+      {children}
+    </motion.div>
+  );
+}
