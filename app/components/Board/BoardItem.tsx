89039c4683377c6a153fb724ffa96d2fbb96b03f
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-14 11:35:47 +0900

    feat: 컴포넌트 분리 적용

--- app/components/Board/BoardItem.tsx
+++ app/components/Board/BoardItem.tsx
@@ -1,25 +1,10 @@
-import { useBoardsStore, useTodoStore } from "@/app/store";
+import { useBoardsStore } from "@/app/store";
 import { AnimatePresence } from "motion/react";
-import {
-  closestCenter,
-  DndContext,
-  DragEndEvent,
-  KeyboardSensor,
-  PointerSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
 
-import TodoItem from "../Todo";
 import TodoCreateButton from "../Todo/TodoCreateButton";
 import BoardTitle from "./BoardTitle";
 import BoardItemWrapper from "./BoardItemWrapper";
+import TodoItems from "../Todo";
 
 interface BoardItemProps {
   boardId: string;
@@ -31,55 +16,13 @@ export default function BoardItem({ boardId }: BoardItemProps) {
       return state.boards.find((board) => board.id === boardId);
     }
   });
-  const todos = useTodoStore((state) => state.todos);
-  const todoArr = board?.todoIds?.map((id) => ({ ...todos[id], id }));
-  const updateTodo = useBoardsStore((state) => state.updateTodo);
-
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (active.id === over?.id) return;
-    if (over && over.id) {
-      console.log(active.id, over.id);
-      console.log("실행");
-      updateTodo(boardId, active.id.toString(), over?.id.toString());
-    }
-  };
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(TouchSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
 
   return (
     <AnimatePresence>
       {board?.isExisting && (
         <BoardItemWrapper color={board?.color}>
           <BoardTitle boardId={boardId} />
-          <AnimatePresence>
-            <DndContext
-              sensors={sensors}
-              onDragEnd={handleDragEnd}
-              collisionDetection={closestCenter}
-            >
-              {todoArr && (
-                <SortableContext
-                  items={todoArr}
-                  strategy={verticalListSortingStrategy}
-                >
-                  {todoArr?.map(
-                    (item) =>
-                      item.isExisting && (
-                        <TodoItem key={item.id} todoId={item.id} />
-                      ),
-                  )}
-                </SortableContext>
-              )}
-            </DndContext>
-          </AnimatePresence>
+          <TodoItems boardId={boardId} />
           <TodoCreateButton boardId={boardId} />
         </BoardItemWrapper>
       )}
